04279a5ed1f3094e0bbb6220e4f3708aa25923b1
Gourav meena <67853722+GGourav@users.noreply.github.com>
2026-04-17 13:48:10 +0530

    Refactor ZonesDatabase to use private zones map

--- app/src/main/java/com/albionplayersradar/data/ZonesDatabase.kt
+++ app/src/main/java/com/albionplayersradar/data/ZonesDatabase.kt
@@ -7,25 +7,26 @@ object ZonesDatabase {
         val tier: Int
     )
 
-    val zones: Map<String, ZoneInfo> = mapOf(
+    private val zones = mapOf(
         "THETFORD" to ZoneInfo("Thetford", "safe", 4),
         "LYMHURST" to ZoneInfo("Lymhurst", "safe", 3),
         "FORTSTERLING" to ZoneInfo("Fort Sterling", "safe", 2),
         "MARTLOCK" to ZoneInfo("Martlock", "safe", 5),
         "BRIDGEPWATCH" to ZoneInfo("Bridgewatch", "safe", 6),
-        "CARLEON" to ZoneInfo("Carleon", "black", 8),
-        "MERLYN" to ZoneInfo("Merlyn", "safe", 4),
-        "MARCH" to ZoneInfo("March", "safe", 3),
+        "CARLEON" to ZoneInfo("Caerleon", "black", 8),
+        "THESTONE" to ZoneInfo("The Stone", "safe", 1),
+        "MERLYN" to ZoneInfo("Merlyn", "safe", 3),
         "BLACKROCK" to ZoneInfo("Blackrock", "red", 6),
         "CAERLEON" to ZoneInfo("Caerleon Roads", "black", 8),
-        "THESTONE-1" to ZoneInfo("The Stone", "safe", 0),
-        "THESTONE-2" to ZoneInfo("The Stone", "safe", 0),
-        "BRITTAIN" to ZoneInfo("Brittain", "safe", 0)
+        "ROAD" to ZoneInfo("Royal Roads", "red", 7)
     )
 
     fun getZone(id: String): ZoneInfo? = zones[id]
 
-    fun getPvpType(id: String): String = getZone(id)?.pvpType ?: "safe"
+    fun getPvpType(id: String): String = zones[id]?.pvpType ?: "safe"
 
-    fun isDangerous(id: String): Boolean = getPvpType(id) in listOf("black", "red")
+    fun isDangerous(id: String): Boolean {
+        val pvp = getPvpType(id)
+        return pvp == "black" || pvp == "red"
+    }
 }
